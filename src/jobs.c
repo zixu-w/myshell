754f3c91b2aa8e9913cef06336ddc898cd66e5ff
WANG Zixu <wangzixu.china@gmail.com>
2016-09-22 11:10:30 +0800

    Fixed error message.

--- src/jobs.c
+++ src/jobs.c
@@ -1,19 +1,24 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <error.h>
 #include <sys/wait.h>
 
 #include "jobs.h"
+
+extern char* program_invocation_name;
+extern char* program_invocation_short_name;
 
 int launch(Command_t** commands) {
   Command_t command = *commands[0];
   pid_t pid;
   int status;
+  program_invocation_name = program_invocation_short_name;
   pid = fork();
   if (pid == 0) {
     if (execvp(command.cmd, command.argv) == -1) {
-      perror("myshell");
-      exit(EXIT_FAILURE);
+      error(EXIT_FAILURE, errno, "'%s'", command.cmd);
     }
   } else if (pid < 0) {
     perror("myshell");
